543eb98fb1b61cb1ec666f44f59984975935fb80
best.neil@prophecy.io <best.neil@prophecy.io>
2025-04-10 15:10:26 +0000

    Refactor IOT_BATCHES model for incremental loading with merge strategy and schema updates

--- models/IOT_BATCHES.sql
+++ models/IOT_BATCHES.sql
@@ -4,6 +4,15 @@
   })
 }}
 
+{{
+  config({    
+    "materialized": "incremental",
+    "incremental_strategy": "merge",
+    "on_schema_change": 'fail',
+    "unique_key": ["LOADED_AT", "DATE"]
+  })
+}}
+
 WITH IOT AS (
 
   SELECT * 
@@ -18,8 +27,49 @@ IOT_BATCHES_1 AS (
   
   FROM {{ source('ONBE_DEMO_DEV.PUBLIC', 'IOT_BATCHES') }}
 
+),
+
+iot_incremental AS (
+
+  SELECT *
+  
+  FROM IOT
+  
+  {% if is_incremental() %}
+    WHERE 
+      LOADED_AT > (
+        SELECT COALESCE( MAX(LOADED_AT), '1900-01-01')
+        FROM IOT_BATCHES_1
+  {% endif %}
+
+),
+
+count_by_date AS (
+
+  SELECT 
+    LOADED_AT AS LOADED_AT,
+    DATE(ts) AS DATE,
+    COUNT(*) AS N_RECORDS_LOADED
+  
+  FROM iot_incremental
+  
+  GROUP BY 
+    LOADED_AT, DATE(TS)
+
+),
+
+reported_at AS (
+
+  SELECT 
+    LOADED_AT,
+    DATE,
+    N_RECORDS_LOADED,
+    current_timestamp() AS REPORTED_AT
+  
+  FROM count_by_date
+
 )
 
 SELECT *
 
-FROM IOT
+FROM reported_at

554120397a19ee8fa7d7b05af0c0ae33dc6de1a3
best.neil@prophecy.io <best.neil@prophecy.io>
2025-04-10 14:00:53 +0000

    Refactor iot_batches model for incremental loading and schema updates - Simplify incremental logic and remove redundant SQL statements - Update schema.yml to reflect new model structure and data types

--- models/iot_batches.sql
+++ models/iot_batches.sql
@@ -1,12 +1,58 @@
 {{
   config({    
     "materialized": "incremental",
+    "alias": "IOT_BATCHES",
     "incremental_predicates": [],
     "incremental_strategy": "merge",
     "on_schema_change": 'append_new_columns',
     "unique_key": ["LOADED_AT", "YEAR", "MONTH"]
   })
 }}
+
+WITH iot_incremental AS (
+
+  SELECT *
+  
+  FROM IOT
+  
+  {% if is_incremental() %}
+    WHERE 
+      LOADED_AT > (
+        SELECT COALESCE( MAX(LOADED_AT), '1900-01-01')
+        FROM IOT_BATCHES
+  {% endif %}
+
+),
+
+count_by_date AS (
+
+  SELECT 
+    LOADED_AT AS LOADED_AT,
+    DATE(ts) AS DATE,
+    COUNT(*) AS N_RECORDS_LOADED
+  
+  FROM iot_incremental
+  
+  GROUP BY 
+    LOADED_AT, DATE(TS)
+
+),
+
+reported_at AS (
+
+  SELECT 
+    LOADED_AT,
+    DATE,
+    N_RECORDS_LOADED,
+    current_timestamp() AS REPORTED_AT
+  
+  FROM count_by_date
+
+)
+
+SELECT *
+
+FROM reported_at
 
 WITH IOT AS (
 
@@ -22,69 +68,5 @@ IOT_BATCHES AS (
   
   FROM {{ source('ONBE_DEMO_DEV.PUBLIC', 'IOT_BATCHES') }}
 
-),
-
-max_loaded_at AS (
-
-  {#Identifies the most recent data load timestamp from IoT batches.#}
-  SELECT 
-    MAX(LOADED_AT) AS max_loaded_at
-  
-  FROM IOT_BATCHES AS in0
-
-),
-
-default_loaded_at AS (
-
-  {#Determines the most recent loading date, defaulting to a historical date if no data is available.#}
-  SELECT coalesce(max_loaded_at, '1900-01-01') AS max_loaded_at
-  
-  FROM max_loaded_at AS in0
-
-),
-
-SQLStatement_1 AS (
-
-  SELECT *
-  
-  FROM IOT
-  
-  {% if is_incremental() %}
-    WHERE 
-      LOADED_AT > (SELECT max_loaded_at FROM default_loaded_at)
-  {% endif %}
-
-),
-
-count_by_date AS (
-
-  {#Tracks the number of records loaded over time, organized by year and month.#}
-  SELECT 
-    LOADED_AT AS LOADED_AT,
-    date( ts) AS date,
-    COUNT(*) AS N_RECORDS_LOADED
-  
-  FROM SQLStatement_1 AS in0
-  
-  GROUP BY 
-    LOADED_AT, YEAR(TS), MONTH(TS)
-
-),
-
-reported_at AS (
-
-  {#Formats data to include loading timestamps and record counts for reporting.#}
-  SELECT 
-    COALESCE(LOADED_AT, '1900-01-01') AS LOADED_AT,
-    YEAR AS YEAR,
-    MONTH AS MONTH,
-    N_RECORDS_LOADED AS N_RECORDS_LOADED,
-    current_timestamp() AS REPORTED_AT
-  
-  FROM count_by_date AS in0
-
 )
 
-SELECT *
-
-FROM reported_at

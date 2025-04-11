c2cc0d7730c09ae5b0900d99e4f1f6e0c97ff786
Neil Best <neil.best@Neils-MacBook-Pro.local>
2025-04-11 09:28:15 -0500

    reduce indentation and delete useless code

--- macros/log_results.sql
+++ macros/log_results.sql
@@ -1,108 +1,77 @@
 {% macro log_results(results) %}
 
-    {% if execute %}
+{% if execute %}
 
-        {{ log("updating audit table", info=True) }}
+{{ log("updating audit table", info=True) }}
 
-        {%- set load_log_values -%}
+{%- set load_log_values -%}
 
-          {%- for r in results -%}
-            {{ [ r.node.relation_name,
-                 r.adapter_response.query_id,
-                 r.timing|map(attribute="started_at")|min|string,
-                 r.timing|map(attribute="completed_at")|max|string,
-                 r.status.value|string,
-                 r.message
-               ] | join("',\n'")
-            }}
-          {%- endfor -%}
-        {%- endset -%}
+  {%- for r in results -%}
+    {{ [ r.node.relation_name,
+         r.adapter_response.query_id,
+         r.timing|map(attribute="started_at")|min|string,
+         r.timing|map(attribute="completed_at")|max|string,
+         r.status.value|string,
+         r.message
+       ] | join("',\n'")
+    }}
+  {%- endfor -%}
+{%- endset -%}
 
-        {{ print( load_log_values ) }}
+{{ print( load_log_values ) }}
 
-        {%- set load_log_insert -%}
-            INSERT INTO ONBE_DEMO_DEV.EDW_LOAD_LOG (
-                EDW_TABLE_NAME,
-                LOAD_START,
-                LOAD_END,
-                LOAD_STATUS,
-                ERROR_MESSAGE,
-                CREATE_DATE,
-                UPDATE_DATE,
-                CREATED_BY_USER )
-            VALUES
-            ( '{{ [ load_log_values ]|join(",\n") }}' );
-        {% endset %}
+{%- set load_log_insert -%}
+    INSERT INTO ONBE_DEMO_DEV.EDW_LOAD_LOG (
+        EDW_TABLE_NAME,
+        LOAD_START,
+        LOAD_END,
+        LOAD_STATUS,
+        ERROR_MESSAGE,
+        CREATE_DATE,
+        UPDATE_DATE,
+        CREATED_BY_USER )
+    VALUES
+    ( '{{ [ load_log_values ]|join(",\n") }}' );
+{% endset %}
 
-        {{ print( load_log_insert) }}
+{{ print( load_log_insert) }}
 
-        {{ print( "result:\n" ~ results|pprint) }}
+{{ print( "result:\n" ~ results|pprint) }}
 
 
 {#
 
-            {{- log("operated on " ~ r.adapter_response.rows_affected ~ " rows", info=True) -}}
+{{- log("operated on " ~ r.adapter_response.rows_affected ~ " rows", info=True) -}}
 
-            SELECT
-                -- SPLIT_PART('{{ res.unique_id }}', '.', -1)
-                '{{ res.node.relation_name }}'
-                  AS edw_table_name,
-                '{{ res.adapter_response.query_id }}'
-                  AS query_id,
-                -- DATEADD(SECOND, -{{ res.execution_time }}, CURRENT_TIMESTAMP())
-                '{{ res.timing|map(attribute="started_at")|min }}'
-                  AS load_start,
-                -- CURRENT_TIMESTAMP()
-                '{{ res.timing|map(attribute="completed_at")|max }}'
-                  AS load_end,
-                '{{ res.status }}'
-                  AS load_status,
-                '{{ res.message }}'
-                  AS load_message
-            {% endset %}
-  
-            {% set load_log_record = run_query(load_log_select) %}
+{% set load_log_record = run_query(load_log_select) %}
 
-            {% do load_log_record.print_json(indent=2) %}
+{% do load_log_record.print_json(indent=2) %}
 
-            {% set load_log_query %}
-            INSERT INTO EDW_LOAD_LOG (
-                EDW_TABLE_NAME, LOAD_START, LOAD_END, LOAD_STATUS, ERROR_MESSAGE, CREATE_DATE, UPDATE_DATE, CREATED_BY_USER)
-            VALUES (
-                SPLIT_PART('{{ res.node.unique_id }}', '.', -1),
-                DATEADD(SECOND, -{{ res.execution_time }}, CURRENT_TIMESTAMP()),
-                CURRENT_TIMESTAMP(),
-                '{{ res.status }}',
-                '{{ res.message }}',
-                CURRENT_TIMESTAMP(),
-                CURRENT_TIMESTAMP(),
-                CURRENT_USER)
-            {% endset %}
 
-            {% set run_id_query %}
-            SELECT RUN_ID FROM TABLE(RESULT_SCAN(LAST_QUERY_ID()))
-            {% endset %}
-  
-            {% set run_id_result = run_query(run_id_query) %}
-            {% set run_id = run_id_result['data'][0][0] %}
+{% set run_id_query %}
+SELECT RUN_ID FROM TABLE(RESULT_SCAN(LAST_QUERY_ID()))
+{% endset %}
 
-            {% if res.status != 'success' %}
-                {% set error_log_query %}
-                INSERT INTO AUDIT.EDW_ERROR_LOG (
-                    ERROR_MESSAGE, ORIGINAL_RUN_ID)
-                VALUES (
-                    '{{ res.message }}', '{{ run_id }}')
-                {% endset %}
-    
-                {% do run_query(error_log_query) %}
-            {% endif %}
+{% set run_id_result = run_query(run_id_query) %}
+{% set run_id = run_id_result['data'][0][0] %}
 
-            {{ log("Inserted row with RUN_ID: " ~ run_id, info=True) }}
+{% if res.status != 'success' %}
+    {% set error_log_query %}
+    INSERT INTO AUDIT.EDW_ERROR_LOG (
+        ERROR_MESSAGE, ORIGINAL_RUN_ID)
+    VALUES (
+        '{{ res.message }}', '{{ run_id }}')
+    {% endset %}
 
-            #}
+    {% do run_query(error_log_query) %}
+{% endif %}
 
-        {{- log("finished updating audit table", info=True) -}}
+{{ log("Inserted row with RUN_ID: " ~ run_id, info=True) }}
 
-    {%- endif -%}
+#}
+
+{{- log("finished updating audit table", info=True) -}}
+
+{%- endif -%}
 
 {%- endmacro -%}

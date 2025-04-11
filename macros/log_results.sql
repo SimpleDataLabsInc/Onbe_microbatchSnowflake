3c62f28befd917894c38a890f1413c9133c09a58
Neil Best <neil.best@Neils-MacBook-Pro.local>
2025-04-11 17:24:41 -0500

    cleaned up `log_results()` macro real nice

--- macros/log_results.sql
+++ macros/log_results.sql
@@ -1,51 +1,91 @@
-{% macro log_results(results) %}
+{% macro log_results(results) -%}
 
-{% if execute %}
+{%- if execute -%}
 
-{{ log("updating audit table", info=True) }}
+    {%- set load_log_table -%}
+ONBE_DEMO_{{- var('DBT_TARGET') -}}.PUBLIC.DBT_LOAD_LOG
+    {%- endset -%}
 
-{%- set load_log_values -%}
-
-  {%- for r in results -%}
-    {{ [ r.node.relation_name,
-         r.adapter_response.query_id,
-         r.timing|map(attribute="started_at")|min|string,
-         r.timing|map(attribute="completed_at")|max|string,
-         r.status.value|string,
-         r.message
-       ] | join("',\n'")
-    }}
-  {%- endfor -%}
-{%- endset -%}
-
-{{ print( load_log_values ) }}
-
-{%- set load_log_insert -%}
-    INSERT INTO ONBE_DEMO_DEV.EDW_LOAD_LOG (
-        EDW_TABLE_NAME,
-        LOAD_START,
-        LOAD_END,
-        LOAD_STATUS,
-        ERROR_MESSAGE,
-        CREATE_DATE,
-        UPDATE_DATE,
-        CREATED_BY_USER )
-    VALUES
-    ( '{{ [ load_log_values ]|join(",\n") }}' );
-{% endset %}
-
-{{ print( load_log_insert) }}
-
-{{ print( "result:\n" ~ results|pprint) }}
+    {{- log( 'Updating audit table ' ~ load_log_table, info=True) -}}
+    {{- log( 'DBT invocation ID is ' ~ invocation_id, info=True ) -}}
 
 
-{#
+    {% for r in results -%}
+       {{ log( 'DBT Model "' ~
+               r.node.relation_name ~
+               '" operated on ' ~
+               r.adapter_response.rows_affected ~
+               ' rows.', info=True) }}
+    {% endfor -%}
 
-{{- log("operated on " ~ r.adapter_response.rows_affected ~ " rows", info=True) -}}
+    {% set load_log_insert %}
 
-{% set load_log_record = run_query(load_log_select) %}
+INSERT INTO {{ load_log_table }} (
+    DBT_INVOCATION_ID,
+    SNOWFLAKE_QUERY_ID,
+    DBT_RELATION_NAME,
+    LOAD_START_TS,
+    LOAD_END_TS,
+    LOAD_STATUS,
+    RESULT_MESSAGE,
+    N_ROWS_AFFECTED,
+    CREATED_AT_TS,
+    UPDATED_AT_TS,
+    CREATED_BY_USER )
+VALUES
 
-{% do load_log_record.print_json(indent=2) %}
+        {%- for r in results %}
+(
+    '{{ invocation_id }}',
+    '{{ r.adapter_response.query_id }}',
+    '{{ r.node.relation_name }}',
+    '{{ r.timing|map(attribute="started_at")|min|string }}',
+    '{{ r.timing|map(attribute="completed_at")|max|string }}',
+    '{{ r.status.value|string }}',
+    '{{ r.message }}',
+    {{ r.adapter_response.rows_affected }},
+    CURRENT_TIMESTAMP(),
+    CURRENT_TIMESTAMP(),
+    CURRENT_USER()
+)
+
+            {%- if not loop.last %},
+            {%- else %};
+            {%- endif %}
+
+        {%- endfor %}
+
+    {%- endset %}
+
+    {{ print( load_log_insert) }}
+
+    {%- do run_query( load_log_insert) -%}
+
+    {{ log( 'Audit table update complete', info=True) }}
+
+    {#
+
+    {{ print( "Run results:\n\n" ~ results|pprint) }}
+
+    #}
+
+
+{%- endif -%}
+
+{%- endmacro -%}
+
+
+
+{#     everything below is commented out
+
+{% do run_query( load_log_insert) %}
+
+.print_json(indent=2) %}
+
+
+
+
+
 
 
 {% set run_id_query %}
@@ -70,8 +110,3 @@ SELECT RUN_ID FROM TABLE(RESULT_SCAN(LAST_QUERY_ID()))
 
 #}
 
-{{- log("finished updating audit table", info=True) -}}
-
-{%- endif -%}
-
-{%- endmacro -%}

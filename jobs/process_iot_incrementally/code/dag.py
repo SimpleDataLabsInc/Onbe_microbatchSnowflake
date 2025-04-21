67cb08e8f20694bc3edf343aa5a5c303b0cefe30
best.neil@prophecy.io <best.neil@prophecy.io>
2025-04-21 23:27:11 +0000

    Change DBT_TARGET from PROD to DEV in process_iot_incrementally job configuration

--- jobs/process_iot_incrementally/code/dag.py
+++ jobs/process_iot_incrementally/code/dag.py
@@ -14,7 +14,7 @@ with DAG(
     dag_id = "Onbe_microbatchSnowflake_process_iot_incrementally", 
     schedule_interval = "0/10 * * * *", 
     default_args = {"owner" : "Prophecy", "retries" : 0, "ignore_first_depends_on_past" : True, "do_xcom_push" : True}, 
-    params = {'DBT_TARGET' : Param("""PROD""", type = "string", title = """DBT_TARGET""")}, 
+    params = {'DBT_TARGET' : Param("""DEV""", type = "string", title = """DBT_TARGET""")}, 
     start_date = pendulum.today('UTC'), 
     catchup = False, 
     max_active_runs = 1
